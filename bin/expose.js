e980113a221c39b3b3705689339c2b28bfd240c6
robinmonjo <robinmonjo@gmail.com>
2022-07-11 14:15:21 +0200

    basic expose command

--- bin/expose.js
+++ bin/expose.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+const startNgrok = require("../lib/ngrok").startNgrok;
+const startLocalServer = require("../lib/local_server").startLocalServer;
+
 console.log("expose");
+
+startNgrok((host) => {
+  console.log(`NGROK host is ${host}`);
+
+  startLocalServer();
+  console.log("local server running");
+});
