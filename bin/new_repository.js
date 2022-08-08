4c83e62f82cc2934cc5297ec3b17904dbb2b35ff
robinmonjo <robinmonjo@gmail.com>
2022-08-08 11:32:11 +0200

    create git repository automatically

--- bin/new_repository.js
+++ bin/new_repository.js
@@ -1,3 +1,12 @@
 const createEmptyRepository = require("../lib/new_repository/create_empty_repository");
+const ensureThemeExists = require("../lib/new_repository/ensure_theme_exists");
+const extractBaseTheme = require("../lib/new_repository/extract_base_theme");
 
-createEmptyRepository("grand-conference", "../test-cms-dup-robin")
+const baseTheme = "grand-conference";
+const directory = "../test-cms-dup-robin2/";
+
+ensureThemeExists(baseTheme);
+
+createEmptyRepository(directory, () => {
+  extractBaseTheme(baseTheme, directory)
+});
