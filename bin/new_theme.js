35c778ba0bda7aea10ece483f8f427951ec85db7
robinmonjo <robinmonjo@gmail.com>
2022-08-08 14:54:14 +0200

    create new theme from another one

---
+++ bin/new_theme.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const createTheme = require("../lib/new_theme/create_theme");
+const { logSuccess } = require("../lib/utils/log");
+
+const argv = yargs(hideBin(process.argv))
+  .options({
+    "theme": {
+      alias: "n",
+      describe : "The name of the new theme",
+      demandOption: true
+    },
+    "baseTheme": {
+      alias: "b",
+      describe: "The name of the theme to use as a base",
+      demandOption: true
+    }
+  })
+  .config()
+  .env("ETM")
+  .help("help")
+  .wrap(null)
+  .argv;
+
+const {
+  theme,
+  baseTheme
+} = argv;
+
+createTheme(theme, baseTheme);
+
+logSuccess(`Theme ${theme} is ready 🚀\nMake sure to ask the Eventmaker team to allow you to work on this new theme.`);
